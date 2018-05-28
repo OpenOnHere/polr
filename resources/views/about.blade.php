bdddf1b0f8fdd4556eafb8cd768f769f87e8303c
Zhijun <chenzj@yidoutang.com>
2018-05-28 17:04:30 +0800

    about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,6 +6,43 @@
 @endsection
 
 @section('content')
+<style type="text/css">
+    .logo-well{
+        width: 10em;
+    }
+    .logo-img{
+        width: 7em;
+    }
+    .self-container{
+        width: 56em;
+        margin-bottom: 30px;
+    }
+    .self-container .qrcode{
+        height: 15em;
+    }
+    .self-text{
+        font-size: 16px;
+    }
+</style>
+<div class="self-container">
+    <h1>is.app</h1>
+    <p class="self-text">
+        is app 短链接，顾名思义，转向必是app，特别适用于app的分享。因为app域名刚刚开放申请不到一个月，我姑且妄称app域名第一短链接，我想app域名做短链接几乎没有比这个更合适的了。
+        按照我的计划可能随后还会尝试推出前缀形式的短链接，比如app.is.app这样的形式，这大概不符合备案制度，后面会想办法规避。既然开弓必没有回头箭，大家的支持就是我前行的动力。
+    </p>
+    <p class="self-text">
+        现在你们看到的是一个公测版，它完全基于Polr搭建而来。服务端使用最基本的LNMP架构，虽然使用了<a target="_blank" href="https://lumen.laravel.com/">为速度而生的Lumen框架</a>，但是我觉得性能上应该还有发挥的余地。如果程序猿同道有单机提高性能的意见，还请不吝赐教，毕竟成本高昂。
+    </p>
+    <p class="self-text">
+        公测期间申请的链接将长期有效，同时开放了自定义申请，如果大家没有抢到钟意的域名，抢一个短链接也不错^_^。但是也请大家注意：
+    </p>
+    <h4 style="color: red;">非法链接一律删除！！！恶意批量申请一律删除！！！</h4>
+    <p class="self-text">
+        大家如果喜欢这个产品，可以小小的支持一下，请我喝一杯咖啡。<br/>
+        <img class="qrcode" src="/img/alipay.jpeg">
+        <img class="qrcode" src="/img/wechat.png">
+    </p>
+</div>
 <div class='well logo-well'>
     <img class='logo-img' src='/img/logo.png' />
 </div>
